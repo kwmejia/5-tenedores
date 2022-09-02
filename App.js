82b560a84563f43331589b610db04e7007628064
Kevin Mejía <kwmejia9@gmail.com>
2022-09-02 11:13:22 -0500

    Solucionado el Error de bael

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Navigation } from "./app/navigations/";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello word</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ app/screens/index.js
@@ -0,0 +1,5 @@
+export * from "./Account";
+export * from "./Restaurants";
+export * from "./Favorites";
+export * from "./Search";
+export * from "./TopRestaurants";
